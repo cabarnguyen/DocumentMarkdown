846b05663874d21a784f4bef807db54ab660c1ca
cabarnguyen <mr.thanhca@gmail.com>
2025-02-07 12:30:53 +0700

    m

--- Services/MarkdownService.cs
+++ Services/MarkdownService.cs
@@ -5,7 +5,7 @@ namespace DocumentMarkdown.Services
     public class MarkdownService
     {
         private readonly MarkdownPipeline _pipeline;
-
+        //
         public MarkdownService()
         {
             _pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
